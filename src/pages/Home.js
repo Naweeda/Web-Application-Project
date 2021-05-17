5028fc3127e4b76508caccac5e4ef28261337c2b
RobertAldenFreeman <robertfreeman6092@gmail.com>
2021-05-16 17:54:49 -0700

    view/post call-to-action and about section added to homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './pages.css';
 import './map.css';
+
 import currentUser from '../components/currentUser'; // gets current user
 
 const Home = () => {
@@ -8,8 +9,33 @@ const Home = () => {
         <div>
             <h2>Home</h2>
             {currentUser.getUser().isLoggedIn && (
-                <h3>Greetings, {currentUser.getUser().name}!</h3>
+                <h5>Greetings, {currentUser.getUser().name}!</h5>
             )}
+
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="card w-100">
+                    <div class="card-body">
+                        <h6 class="card-title">Buy</h6>
+                        <p class="card-text">See recent listings</p>
+                        <a href="/userlistings" class="btn btn-primary">View items</a>
+                    </div>
+                    </div>
+                </div>
+
+            <div class="col-sm-6">
+                <div class="card w-100">
+                <div class="card-body">
+                    <h6 class="card-title">Sell</h6>
+                    <p class="card-text">See recent listings</p>
+                    <a href="/adminpost" class="btn btn-primary">Post an item</a>
+                </div>
+                </div>
+            </div>
+            </div>
+
+
+            <br></br>
             <h4>Our Location</h4>
             <div class="mapouter">
                 <div class="gmap_canvas">
@@ -19,6 +45,13 @@ const Home = () => {
                         <a href="https://www.embedgooglemap.net"></a>
                 </div>
             </div>
+            <br></br>
+            <h4>
+                About
+            </h4>
+            <p>
+                Create listings, view listings and chat with listing posters! This site is a chatty way to buy and sell items.
+            </p>
         </div>
     )
 }
